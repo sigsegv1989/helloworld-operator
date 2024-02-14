64aee3002d5b63466ef2060c6a7267ba8c38c027
Abhijeet Rokade <sigsegv1989@gmail.com>
2024-02-14 10:34:34 +0000

    Deploy the centos image deployment with replicas count fron the HelloWorld CR.
    Update the operator reconcile func to deploy the Centos deployment with replicas count from the HelloWorld CR.

--- internal/controller/helloworld_controller.go
+++ internal/controller/helloworld_controller.go
@@ -19,10 +19,15 @@ package controller
 import (
 	"context"
 
+	appsv1 "k8s.io/api/apps/v1"
+	corev1 "k8s.io/api/core/v1"
+	"k8s.io/apimachinery/pkg/api/errors"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
+	"k8s.io/apimachinery/pkg/types"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
-	"sigs.k8s.io/controller-runtime/pkg/log"
+	ctrllog "sigs.k8s.io/controller-runtime/pkg/log"
 
 	helloworldv1 "helloworld-operator/api/v1"
 )
@@ -47,11 +52,87 @@ type HelloWorldReconciler struct {
 // For more details, check Reconcile and its Result here:
 // - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.15.0/pkg/reconcile
 func (r *HelloWorldReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
-	_ = log.FromContext(ctx)
+	log := ctrllog.FromContext(ctx)
+	// Fetch the HelloWorld instance
+	operator := &helloworldv1.HelloWorld{}
+	err := r.Get(ctx, req.NamespacedName, operator)
 
-	// TODO(user): your logic here
+	if err != nil {
+		if errors.IsNotFound(err) {
+			// Request object not found, could have been deleted after reconcile request.
+			// Owned objects are automatically garbage collected. For additional cleanup logic use finalizers.
+			// Return and don't requeue
+			log.Info("HelloWorld CR not found. Ignoring since object must be deleted")
+			return ctrl.Result{}, nil
+		}
+		// Error reading the object - requeue the request.
+		log.Error(err, "Failed to get HelloWorld CR")
+		return ctrl.Result{}, err
+	}
+
+	// Check if the deployment already exists, if not create a new one
+	found := &appsv1.Deployment{}
+	err = r.Get(ctx, types.NamespacedName{Name: operator.Name, Namespace: operator.Namespace}, found)
+	if err != nil && errors.IsNotFound(err) {
+		// Define a new deployment
+		dep := r.deploymentForOperator(operator)
+		log.Info("Creating a new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+		err = r.Create(ctx, dep)
+		if err != nil {
+			log.Error(err, "Failed to create new Deployment", "Deployment.Namespace", dep.Namespace, "Deployment.Name", dep.Name)
+			return ctrl.Result{}, err
+		}
+		// Deployment created successfully - return and requeue
+		return ctrl.Result{Requeue: true}, nil
+	} else if err != nil {
+		log.Error(err, "Failed to get Deployment")
+		return ctrl.Result{}, err
+	}
 
 	return ctrl.Result{}, nil
+}
+
+// labelsForOperator returns the labels for selecting the resources
+// belonging to the given operator CR name.
+func labelsForOperator(name string) map[string]string {
+	return map[string]string{"app": "hello-world", "operator_cr": name}
+}
+
+func (r *HelloWorldReconciler) deploymentForOperator(m *helloworldv1.HelloWorld) *appsv1.Deployment {
+	replicas := m.Spec.Replicas
+	ls := labelsForOperator(m.Name)
+
+	dep := &appsv1.Deployment{
+		ObjectMeta: metav1.ObjectMeta{
+			Name:      m.Name,
+			Namespace: m.Namespace,
+		},
+		Spec: appsv1.DeploymentSpec{
+			Replicas: &replicas,
+			Selector: &metav1.LabelSelector{
+				MatchLabels: ls,
+			},
+			Template: corev1.PodTemplateSpec{
+				ObjectMeta: metav1.ObjectMeta{
+					Labels: ls,
+				},
+				Spec: corev1.PodSpec{
+					Containers: []corev1.Container{
+						{
+							Image:           "centos:latest",
+							ImagePullPolicy: corev1.PullAlways,
+							Name:            "centos-container",
+							Command:         []string{"/bin/sleep", "3600"},
+						},
+					},
+				},
+			},
+		},
+	}
+
+	// Set Operator instance as the owner and controller
+	ctrl.SetControllerReference(m, dep, r.Scheme)
+	return dep
 }
 
 // SetupWithManager sets up the controller with the Manager.
